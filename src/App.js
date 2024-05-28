69b6cd65d7035e32d839bc043feca6e2e8d6ab32
Satyam <satyam19arya@gmail.com>
2024-05-29 02:38:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 function App() {
   return (
     <div>
+      <p className="text-red-700 p-2 px-6">Note: I have deployed the backend on Render (free tier). Please be aware that Render spins down a free web service if it goes 15 minutes without receiving inbound traffic. Therefore, you might need to wait 30-40 seconds for it to spin back up and then use the calculator. Thank you for your patience.</p>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
